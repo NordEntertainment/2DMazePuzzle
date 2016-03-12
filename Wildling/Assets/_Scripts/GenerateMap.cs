a38f7d4fa9482e1686dc79a9beec0db0ff6e4a41
Fryden <Fryden@Jrgens-MacBook-Pro.local>
2016-03-12 20:10:21 +0100

    Continued on map generation
    A simple working state

--- Wildling/Assets/_Scripts/GenerateMap.cs
+++ Wildling/Assets/_Scripts/GenerateMap.cs
@@ -18,19 +18,27 @@ public class GenerateMap : MonoBehaviour
 		public tiletype type;
 		public int value;
 		public string tilename;
+		public Sprite tileSprite;
 
-		public void GenerateTileInfo (tiletype til, int val, string nam)
+		public void GenerateTileInfo (tiletype til, int val, string nam, Sprite tiS)
 		{
 			type = til;
 			value = val;
 			tilename = nam;
+			tileSprite = tiS;
 		}
 
 	}
 
 
 	// All Gameobjects
-	public GameObject tile;
+	private GameObject tile;
+
+	// All Sprites
+	private Sprite grassSprite;
+	private Sprite sandSprite;
+	private Sprite stoneSprite;
+
 
 	// All ints
 
@@ -42,7 +50,15 @@ public class GenerateMap : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		tile = Resources.Load ("Prefabs/Tile") as GameObject;
+		grassSprite = Resources.Load<Sprite> ("Materials/Sprites/Grass");
+		sandSprite = Resources.Load<Sprite> ("Materials/Sprites/Sand");
+		stoneSprite = Resources.Load<Sprite> ("Materials/Sprites/Stone");
+
+		print (grassSprite.name);
+
 		GenerateTileList ();
+		GenerateAllTiles ();
 	}
 	
 	// Update is called once per frame
@@ -56,27 +72,55 @@ public class GenerateMap : MonoBehaviour
 		
 		AllTilesInfo = new List<TileInfo> ();
 
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 4000; i++) {
 
 			TileInfo newTile = new TileInfo ();
-			newTile.GenerateTileInfo (tiletype.grass, Random.Range (1, 11), "Grassy Tile");
+			newTile.GenerateTileInfo (tiletype.grass, Random.Range (1, 11), "Grassy Tile", grassSprite);
 			AllTilesInfo.Add (newTile);
 		}
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 3000; i++) {
 
 			TileInfo newTile = new TileInfo ();
-			newTile.GenerateTileInfo (tiletype.sand, Random.Range (1, 11), "Sandy Tile");
+			newTile.GenerateTileInfo (tiletype.sand, Random.Range (1, 11), "Sandy Tile", sandSprite);
 			AllTilesInfo.Add (newTile);
 		}
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 3000; i++) {
 
 			TileInfo newTile = new TileInfo ();
-			newTile.GenerateTileInfo (tiletype.stone, Random.Range (1, 11), "Stony Tile");
+			newTile.GenerateTileInfo (tiletype.stone, Random.Range (1, 11), "Stony Tile", stoneSprite);
 			AllTilesInfo.Add (newTile);
 		}
 
 		print (AllTilesInfo.Count);
 	}
 
+	void GenerateOneTile (tiletype typ, int val, string nam, Sprite spr, int x, int y)
+	{
+		GameObject go = Instantiate (tile, Vector3.zero, Quaternion.identity) as GameObject;
+		go.GetComponent<TileScript> ()._tiletype = typ;
+		go.GetComponent<TileScript> ().value = val;
+		go.GetComponent<TileScript> ().tilename = nam;
+		go.GetComponent<SpriteRenderer> ().sprite = spr;
+		go.transform.position = new Vector2 (x, y);
+	}
 
+	void GenerateAllTiles ()
+	{
+		int tilessquared = (int)Mathf.Sqrt (AllTilesInfo.Count);
+		print (tilessquared);
+
+		TileInfo tileinfo = new TileInfo ();
+
+
+		for (int x = 0; x < tilessquared; x++) {
+			for (int y = 0; y < tilessquared; y++) {
+
+				int ran = Random.Range (0, AllTilesInfo.Count);
+				tileinfo = AllTilesInfo [ran];
+				GenerateOneTile (tileinfo.type, tileinfo.value, tileinfo.tilename, tileinfo.tileSprite, x * 5, y * 5);
+				AllTilesInfo.Remove (AllTilesInfo [ran]);
+
+			}
+		}
+	}
 }
