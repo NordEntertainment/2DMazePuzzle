58437f8ca2c83d6765a8c04cbea0865843c327ea
knabberen <knabberen@gmail.com>
2016-03-12 17:35:22 +0100

    Movement

--- Wildling/Assets/_Scripts/CharacterMovement.cs
+++ Wildling/Assets/_Scripts/CharacterMovement.cs
@@ -6,36 +6,55 @@ public class CharacterMovement : MonoBehaviour
 
 
 	public float moveSpeed;
+	public float rotationSpeed;
+	public Vector2 Velocity;
 
+	private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetKey (KeyCode.W)) {
-			transform.rotation.Set (0, 0, 0, 0);
-			transform.Translate (Vector2.up * moveSpeed * Time.deltaTime);
-		}
+		CharMove ();
+	}
 
-		if (Input.GetKey (KeyCode.D)) {
-			transform.rotation.Set (0, 0, 0, 0);
-			transform.Translate (Vector2.down * moveSpeed * Time.deltaTime);
-		}
+	void CharMove ()
+	{
 
-		if (Input.GetKey (KeyCode.A)) {
-			transform.rotation.Set (0, 0, 0, 0);
-			transform.Translate (Vector2.left * moveSpeed * Time.deltaTime);
-		}
+		rb2d.MovePosition (rb2d.position + Velocity * Time.deltaTime);
 
-		if (Input.GetKey (KeyCode.W)) {
-			transform.rotation.Set (0, 0, 0, 0);
-			transform.Translate (Vector2.right * moveSpeed * Time.deltaTime);
-		}
+		if (Input.GetKey ("up") || Input.GetKey ("w")) {
 
+			rb2d.AddForce (Vector2.up * moveSpeed);
+			rb2d.MoveRotation (angle: 0);
+
+
+		}
+		if (Input.GetKey ("down") || Input.GetKey ("s")) {
+
+			rb2d.AddForce (Vector2.down * moveSpeed);
+			rb2d.MoveRotation (angle: 180);
+
+
+		}
+		if (Input.GetKey ("right") || Input.GetKey ("d")) {
+
+			rb2d.AddForce (Vector2.right * moveSpeed);
+			rb2d.MoveRotation (angle: 270);
+	
+
+		}
+		if (Input.GetKey ("left") || Input.GetKey ("a")) {
+
+			rb2d.AddForce (Vector2.left * moveSpeed);
+			rb2d.MoveRotation (angle: 90);
+		
+
+		}
 	}
 }
